dd3d8908cb7f3221e9186d13b3d2b6aa185c4b11
Jayesh Suthar <jayesh.suthar@rapido.bike>
2024-09-13 21:14:11 +0530

    update formula to consider v1.0.0 of localiser tool

--- Formula/localiser.rb
+++ Formula/localiser.rb
@@ -1,17 +1,15 @@
 class Localiser < Formula
   desc "CLI tool for processing localisation (converts locale JSON files into JS objects"
   homepage "https://github.com/jayesh83/LocalePush"
-  url "https://github.com/jayesh83/LocalePush/releases/download/v0.0.1/LocalePush-0.0.1.jar"
-  sha256 "de17bb566741207cae05322168c4625b17fdbbf29c6d77a5cf8f063c1985abc4"
-  version "v0.0.1"
-
-  depends_on "openjdk"
+  url "https://github.com/jayesh83/LocalePush/releases/download/v1.0.0/LocalePush-v1.0.0.jar"
+  sha256 "85e10aae9cf0faa27999aa4bfed2529586641c5f9544190b199c589053a18385"
+  version "v1.0.0"
 
   def install
-    libexec.install "LocalePush-0.0.1.jar"
+    libexec.install "LocalePush-v1.0.0.jar"
     (bin/"localiser").write <<~EOS
       #!/bin/bash
-      exec java -jar "#{libexec}/LocalePush-0.0.1.jar" "$@"
+      exec java -jar "#{libexec}/LocalePush-v1.0.0.jar" "$@"
     EOS
   end
 
